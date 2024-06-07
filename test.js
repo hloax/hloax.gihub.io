73783b5aee214843a912922e564a6ab20c88ab1c
hloax <133962553+hloax@users.noreply.github.com>
2024-06-07 11:29:35 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,15 @@
 const chatInput = document.querySelector('.chat-input textarea');
 const sendBtn = document.querySelector('.chat-input span');
 const chatbox = document.querySelector('.chatbox');
+const chatbot = document.querySelector('.chatbot');
+const chatbotToggler = document.querySelector('.chatbot-toggler');
 
 let userMessage;
+const inputInitHeight = chatInput.scrollHeight;
+
+const toggleChatBot = () => {
+    chatbot.style.display === 'none' ? chatbot.style.display = 'block' : chatbot.style.display = 'none';
+}
 
 const createChatLi = (message, className) => {
     const chatLi = document.createElement("li");
@@ -27,11 +34,8 @@ const generateResponse = (incomingChatLi) => {
         })
     }
 
-    console.log("Sending request with options:", requestOptions);
-
     fetch(API_URL, requestOptions)
         .then(res => {
-            console.log("Response headers:", res.headers);
             if (!res.ok) {
                 throw new Error('Network response was not ok');
             }
@@ -40,10 +44,9 @@ const generateResponse = (incomingChatLi) => {
         .then(data => {
             const responseText = data.response;
             messageElement.textContent = responseText;
-            console.log(data);
         })
         .catch((error) => {
-            console.error("Error during fetch:", error);
+            messageElement.classList.add("error");
             messageElement.textContent = "Sorry I had a problem on my side, please try again.";
         })
         .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
@@ -52,6 +55,8 @@ const generateResponse = (incomingChatLi) => {
 const handleChat = () => {
     userMessage = chatInput.value.trim();
     if (!userMessage) return;
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
 
     chatbox.appendChild(createChatLi(userMessage, "outgoing"));
     chatbox.scrollTo(0, chatbox.scrollHeight);
@@ -64,5 +69,17 @@ const handleChat = () => {
         generateResponse(incomingChatLi);
     }, 600);
 }
+
+chatInput.addEventListener('input', () => {
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener('keydown', (e) => {
+    if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleChat();
+    }
+});
 
 sendBtn.addEventListener('click', handleChat);
